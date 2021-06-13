e062b663d8b4eca9cb4e2cbe6474640f9451b514
Edwin Ting <edwining01@users.noreply.github.com>
2021-06-13 16:44:48 +0800

    Fix

--- index.js
+++ index.js
@@ -7,37 +7,44 @@
 var isValidElement = require('react').isValidElement
 var FUNCTION = "function"
 var STRING = "string"
-var FUNCOMP2STR = "return React.createElement"
+var PROTOTYPE = "prototype"
+
+function isReactComponent(component) {
+  return typeof component === FUNCTION
+}
 
 function isClassComponent(component) {
-  return typeof component == FUNCTION && component.prototype.isReactComponent
+  return isReactComponent(component) &&
+    component.hasOwnProperty(PROTOTYPE) &&
+    component.prototype.isReactComponent
 }
 
 function isFunctionComponent(component) {
-  return typeof component == FUNCTION && String(component).includes(FUNCOMP2STR)
-}
-
-function isReactComponent(component) {
-  return isClassComponent(component) || isFunctionComponent(component)
+  return isReactComponent(component) && !component.hasOwnProperty(PROTOTYPE)
 }
 
 function isDOMTypeElement(element) {
-  return isValidElement(element) && typeof element.type == STRING
+  return isValidElement(element) && typeof element.type === STRING
 }
 
 function isCompositeTypeElement(element) {
-  return isValidElement(element) && typeof element.type == FUNCTION
+  return isValidElement(element) && typeof element.type === FUNCTION
 }
 
 var exports = {
   isClassComponent,
   isFunctionComponent,
+  
   isReactComponent,
+  isComponent: isReactComponent,
+  
   isDOMTypeElement,
   isCompositeTypeElement,
-  isCompositeTypeElement,
-  isReactComponent: isValidElement,
+  
   isValidElement,
+  isElement: isValidElement,
 }
 exports.default = exports
+Object.freeze && Object.freeze(exports)
+
 module.exports = exports
